040b1595b8a8ab319a120fccc316da3025bceb87
gpasha <g1ebikpasha@gmail.com>
2021-07-15 02:23:31 +0300

    gihub context 2

--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -1,6 +1,7 @@
 import { GithubContext } from './GithubContext'
 import { useReducer } from 'react'
 import { GithubReducer } from './GithubReducer'
+import { SEARCH_USERS, GET_REPOS, CLEAR_USERS, GET_USER, SET_LOADING } from "../ActionTypes"
 
 export const GithubState = ({ children }) => {
 
@@ -13,9 +14,46 @@ export const GithubState = ({ children }) => {
 
     const [state, dispath] = useReducer(GithubReducer, initialState)
 
+    const search = async value => {
+        setLoading()
+        //...
+
+        dispath({
+            type: SEARCH_USERS,
+            payload: []
+        })
+    }
+
+    const getUser = async name => {
+        setLoading()
+        //...
+
+        dispath({
+            type: GET_USER,
+            payload: {}
+        })
+    }
+
+    const getRepos = async name => {
+        setLoading()
+        //...
+
+        dispath({
+            type: GET_REPOS,
+            payload: []
+        })
+    }
+
+    const clearUser = () =>  dispath({ type: CLEAR_USERS })
+
+    const setLoading = () =>  dispath({ type: SET_LOADING })
+
+    const { user, users, repos, loading } = state
+
     return (
         <GithubContext.Provider value={{
-            
+            search, getUser, getRepos, clearUser, setLoading,
+            user, users, repos, loading
         }}>
             { children }
         </GithubContext.Provider>
